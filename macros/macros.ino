03c734526028f71098fc8d8067efbe9711067252
mudit <mudit31@gmail.com>
2021-01-17 21:23:26 +0100

    adding initial modifiers as an array to better follow dry

--- macros/macros.ino
+++ macros/macros.ino
@@ -9,6 +9,12 @@ int keys[] = {2, 3, 4,
               14, 15, 18, 19};
 int numberTotalKeys = (sizeof(keys) / sizeof(keys[0]));
 
+int modifierSequence[] = {KEY_LEFT_SHIFT, 
+                          KEY_LEFT_CTRL, 
+                          KEY_LEFT_ALT, 
+                          KEY_LEFT_GUI};
+int lenModifierSequence = (sizeof(modifierSequence) / sizeof(modifierSequence[0]));
+
 void setup() {
 
   pinMode(RXLED, OUTPUT);
@@ -39,6 +45,13 @@ void loop() {
   }
 }
 
+void sendModifierSequence () {
+  
+  for (int i = 0; i < lenModifierSequence; i++) {
+    Keyboard.press(modifierSequence[i]);
+  }
+}
+
 void handleKeyStroke (int keyIndex) {
 
   /*
@@ -49,85 +62,51 @@ void handleKeyStroke (int keyIndex) {
      11 10 12 13
   */
 
+  sendModifierSequence();
+
   switch (keyIndex) {
     case 0:
       // shortcut: shift ctrl alt gui 0
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('0');
       break;
 
     case 1:
       // shortcut: shift ctrl alt gui 1
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('1');
       break;
 
     case 2:
       // shortcut: shift ctrl alt gui 2
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('2');
       break;
 
     case 3:
       // shortcut: shift ctrl alt gui 3
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('3');
       break;
 
     case 4:
       // shortcut: shift ctrl alt gui 4
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('4');
       break;
 
     case 5:
       // shortcut: shift ctrl alt gui 5
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('5');
       break;
 
     case 6:
       // shortcut: shift ctrl alt gui 6
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('6');
       break;
 
     case 7:
       // shortcut: shift ctrl alt gui 7
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('7');
       break;
 
     case 8:
       // shortcut: shift ctrl alt gui 8
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('8');
       break;
 
@@ -139,35 +118,21 @@ void handleKeyStroke (int keyIndex) {
 
     case 10:
       // shortcut: shift ctrl alt gui a
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('a');
       break;
 
     case 11:
       // shortcut: shift ctrl alt gui b
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('b');
       break;
 
     case 12:
       // shortcut: shift ctrl alt gui c
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('c');
       break;
 
     case 13:
       // shortcut for locking mac: shift command l
-      Keyboard.press(KEY_LEFT_SHIFT);
-      Keyboard.press(KEY_LEFT_GUI);
       Keyboard.press('l');
       break;
   }
